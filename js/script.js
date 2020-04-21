5854edb5bc5b12089c1e58f73e54983f28f9dc68
Vadym <bazalytsky@gmail.com>
2020-04-21 22:32:15 +0300

    second lesson

--- js/script.js
+++ js/script.js
@@ -6,29 +6,67 @@ let money,
     money = +prompt("Ваш бюджет на месяц", "");
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
     
- let expenses = [];
+ let appData = {
+    budjet:  money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: {}, 
+    savings: false
+};
 
 
-for (let i = 0; i<2; i++){
-let nesseseryExpensesName = prompt ("“Введите обязательную статью расходов в этом месяце”", ""),
-    nesseseryExpensesValue = +prompt ("Во сколько обойдется?", "");
+
+
+// for (let i = 0; i<2; i++){
+// let nesseseryExpensesName = prompt ("“Введите обязательную статью расходов в этом месяце”", ""),
+//     nesseseryExpensesValue = +prompt ("Во сколько обойдется?", "");
     
-    expenses[i] = nesseseryExpensesName + ":" + nesseseryExpensesValue ;
-    alert(expenses[i]);
+//     if (typeof(nesseseryExpensesName) === 'string' && typeof(nesseseryExpensesName) != null && typeof(nesseseryExpensesName) === 'string' && typeof(nesseseryExpensesName != null)) {
+//     appData.expenses[nesseseryExpensesName] = nesseseryExpensesValue;
+
+//     } else {
+//         nesseseryExpensesName = prompt ("“Введите обязательную статью расходов в этом месяце”", ""),
+//         nesseseryExpensesValue = +prompt ("Во сколько обойдется?", "");
+//     }
+    
+// }
+
+
+
+let i = 0;
+// do {
+    
+//     let nesseseryExpensesName = prompt ("“Введите обязательную статью расходов в этом месяце”", ""),
+//         nesseseryExpensesValue = +prompt ("Во сколько обойдется?", "");
+
+//     appData.expenses[nesseseryExpensesName] = nesseseryExpensesValue;
+//     i++;
+
+// }
+// while (i<2);
+
+
+while (i<2){
+
+    let nesseseryExpensesName = prompt ("“Введите обязательную статью расходов в этом месяце”", ""),
+    nesseseryExpensesValue = +prompt ("Во сколько обойдется?", "");
+
+appData.expenses[nesseseryExpensesName] = nesseseryExpensesValue;
+i++;
+
 }
 
 
-alert(expenses);
 
 
 
-let appData = {
-    budjet:  money,
-    timeData: time,
-    expenses: expenses,
-    optionalExpenses: null,
-    income: null, 
-    savings: false
-};
+
+
+
+alert("витрати на день" + appData.budjet/30);
+
+
+
 
 
